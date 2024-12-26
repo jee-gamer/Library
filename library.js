b1a33df2aa3d5114f17edbdfb1b86dfd8d9a7c0b
Jiramate Kedmake <jiramate2548@gmail.com>
2024-12-26 20:23:43 +0700

    Show a list of book title

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const MyLibrary = []
+const bookListDiv = document.querySelector('#bookList')
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -14,3 +15,25 @@ function AddBookToLibrary(book){
     MyLibrary.push(book);
 }
 
+function displayBook(){
+    for (let book of MyLibrary){
+        let bookDiv = document.createElement('div');
+        bookDiv.textContent = book.title;
+        bookDiv.style.width = '200px';
+        bookDiv.style.textAlign = 'left';
+        bookListDiv.appendChild(bookDiv);
+    }
+}
+
+const book1 = new Book('To Kill a Mockingbird', 'Harper Lee', 281, true);
+const book2 = new Book('1984', 'George Orwell', 328, false);
+const book3 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+const book4 = new Book('The Catcher in the Rye', 'J.D. Salinger', 277, false);
+
+// Add books to the library
+AddBookToLibrary(book1);
+AddBookToLibrary(book2);
+AddBookToLibrary(book3);
+AddBookToLibrary(book4);
+
+displayBook();
